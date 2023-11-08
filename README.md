651e1875bddd4e5ad9b6645851a0bc5e7c209d20
Pawel Paradysz <pa.paradysz@gmail.com>
2023-11-08 19:04:44 +0100

    update

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 I wanted to show what data analysis looks like with Julia, so made a quick analysis with detecting clicks.
 
+I took the Sperm whales codas creaks from sound cloud, and cut the last 5 seconds of clicks out of it - with full length it would be cumbersome to plot everything.
+
--- main.jl
+++ main.jl
@@ -4,12 +4,10 @@ audio, fs = wavread("terminal.wav")
 
 wavplay(audio, fs)
 
-audio[:, 1] |> histogram
+# plot #1
+plot(eachindex(audio[:, 1]) ./ fs, audio[:, 1], title="Audio wave plot")
 
-audiolen = length(audio[:, 1]) ./ fs
-plot(eachindex(audio[:, 1]) ./ fs, audio[:, 1])
-
-# these values are arbitrary - 15-20 simply gives enough points for calculated standard deviations to be stable
+# here I pick values that are arbitrary - 15-20 simply gives enough points for calculated standard deviations to be stable
 window = 15
 overlap = 5
 
@@ -22,14 +20,14 @@ adjstds(audio; window, overlap) =
 anyininterval(a; wps, minimalinterval) =
     map(any ∘ (x -> Bool.(x)), arraysplit(a, 1 + Int(ceil(minimalinterval * wps)), Int(ceil(minimalinterval * wps))))
 
-plotsec(a; window) = plot(eachindex(a)u"s" ./ fs * window, a)
+plotsec(a; window, kwargs...) = plot(eachindex(a)u"s" ./ fs * window, a; kwargs...)
 
-# looking at this plot can give idea for threshold
-plotsec(adjstds(audio[:, 1]; window, overlap); window)
+# looking at this plot can give idea for threshold, see plot #2
+plotsec(adjstds(audio[:, 1]; window, overlap); window, title="Window standard deviation")
 threshold = 0.03
 
-# and this for minimal interval between the clicks
-plotsec(adjstds(audio[:, 1]; window, overlap) .> threshold; window)
+# and this for minimal interval between the clicks, see plot #3
+plotsec(adjstds(audio[:, 1]; window, overlap) .> threshold; window, title="Threshold met binary signal")
 minimalinterval = 0.01
 
 function findclicktimes(audio; window, overlap, threshold, minimalinterval)
@@ -40,63 +38,73 @@ function findclicktimes(audio; window, overlap, threshold, minimalinterval)
     clickstarttimes = (eachindex(startsclick)./wps)[startsclick]
     endsclick = map(x -> isone(first(x)) & iszero(last(x)), arraysplit(isclick2, 2, 1))
     clickendtimes = (eachindex(endsclick)./wps)[endsclick]
-    (clickstarttimes .+ clickendtimes) ./ 2
+    length(clickstarttimes) == length(clickendtimes) ?
+    (clickstarttimes .+ clickendtimes) ./ 2 :
+    typeof(clickstarttimes)[]
 end
 
 ct_nofilter = findclicktimes(audio[:, 1]; window, overlap, threshold, minimalinterval)
-ct_nofilter[2] .- ct_nofilter[1]
+# 86 clicks, the last one at 3.59541
 
+# Now I shall try to apply a high pass filter, on google I've found
 # https://www.imeko.org/publications/tc19-Metrosea-2020/IMEKO-TC19-MetroSea-2020-19.pdf
-# gives 8000+Hz for click
-# first 0.25s is noise, so let's check it out
+# which gives 8000+Hz for clicks
+# first 0.25s is noise, so let's check out what frequencies it has:
 pg = periodogram(audio[1:Int(fs * 0.25), 1]; fs)
 # let's filter out 99% of power
 pg.freq[findfirst(cumsum(pg.power) .> 0.99 * sum(pg.power))]
-# 4200Hz way below 8kHz
+# 4200Hz is way below 8kHz, so should be fine
 
 
 hpass = digitalfilter(Highpass(4000; fs), Butterworth(4))
 filtered = filt(hpass, audio[:, 1])
-plotsec(adjstds(filtered; window, overlap); window)
+# plot #4
+plot(eachindex(filtered) ./ fs, filtered, title="Audio wave plot after highpass filter")
+
+# see plot #5.
+plotsec(adjstds(filtered; window, overlap); window, title="Window standard deviation with highpass filter")
 # after filtering it's expected we need a new threshold
 threshold = 0.008
 
 ct_filter = findclicktimes(filtered; window, overlap, threshold, minimalinterval)
+# 85 clicks, the last one at 3.555, i.e. with filter, the 3.59541 one is not detected as click
 
-ct_filter
-ct_nofilter
 
-# interestingly applying filter classified the last sound aat 3.59 as not a coda
-
-# instead of eyeballing thresholds we can plot results for different values
-
-let idxs = 0.01 * 10 .^ (-1:0.1:1), nclicksfound(threshold) = length(findclicktimes(filtered; window, overlap, threshold, minimalinterval))
-    plot(idxs, nclicksfound.(idxs); xscale=:log10)
-end
-
+# instead of eyeballing thresholds we can plot results for different values, this is plot #6
 let idxs = 0.01 * 10 .^ (-1:0.1:1), nclicksfound(threshold) = length(findclicktimes(audio[:, 1]; window, overlap, threshold, minimalinterval))
-    plot(idxs, nclicksfound.(idxs); xscale=:log10)
+    plot(idxs, nclicksfound.(idxs); xscale=:log10, xlabel="threshold", ylabel="#clicks detected", title="without highpass filter")
 end
 
-# this can be used to quickly inspect which clicks are detected correctly and which not
-first_differing = findfirst((!isapprox).(findclicktimes(filtered; window, overlap, threshold=1e-2, minimalinterval), findclicktimes(filtered; window, overlap, threshold=5e-3, minimalinterval)[1:end-1]; atol=minimalinterval / 2))
+# plot #7
+let idxs = 0.01 * 10 .^ (-1:0.1:1), nclicksfound(threshold) = length(findclicktimes(filtered; window, overlap, threshold, minimalinterval))
+    plot(idxs, nclicksfound.(idxs); xscale=:log10, xlabel="threshold", ylabel="#clicks detected", title="with highpass filter")
+end
 
-findclicktimes(filtered; window, overlap, threshold=1e-2, minimalinterval)[first_differing], findclicktimes(filtered; window, overlap, threshold=5e-3, minimalinterval)[first_differing]
-# 0.685s, 0.663s
+# this can be used to quickly inspect when additional click gets detected, whether it was correct
+first_differing = findfirst(map((x, y) -> !isapprox(x, y; atol=minimalinterval), findclicktimes(filtered; window, overlap, threshold=1e-2, minimalinterval), findclicktimes(filtered; window, overlap, threshold=5e-3, minimalinterval)))
+first_differing_left, first_differing_right = minmax(findclicktimes(filtered; window, overlap, threshold=1e-2, minimalinterval)[first_differing], findclicktimes(filtered; window, overlap, threshold=5e-3, minimalinterval)[first_differing])
 
-# but same procedure for minimalinterval makes sense only for the highpass-filtered input
+# in this case the additional click was the little hump, totally not a valid click, see screenshot #1
+plotsec(adjstds(filtered; window, overlap); window, xlim=(first_differing_left - minimalinterval, first_differing_right + minimalinterval))
+
+# similarly for the interval, although it should differ from recording to recording as much as threshold, because threshold depends on signal to noise ratio.
 let idxs = 0.01 * 10 .^ (-1:0.1:1), nclicksfound(minimalinterval) = length(findclicktimes(filtered; window, overlap, threshold, minimalinterval))
     plot(idxs, nclicksfound.(idxs); xscale=:log10)
 end
 
-let idxs = 0.01 * 10 .^ (-1:0.1:1), nclicksfound(minimalinterval) = length(findclicktimes(audio[:, 1]; window, overlap, threshold, minimalinterval))
+let idxs = 0.01 * 10 .^ (-1:0.1:1), nclicksfound(minimalinterval) = length(findclicktimes(audio[:, 1]; window, overlap, threshold=0.03, minimalinterval))
     plot(idxs, nclicksfound.(idxs); xscale=:log10)
 end
 
 clickintervals(clicktimes) = clicktimes[2:end] .- clicktimes[1:end-1]
 
-histogram(clickintervals(ct_filter) .* 750u"m"; bins=100)
-
+# quick histogram of inter-click intervals where x axis is what distance would produce echo of this interval in water, plot #8
 ci_filter = clickintervals(ct_filter)
+histogram(ci_filter .* 750u"m"; bins=100, title="Distribution of intervals in meters of echo in water")
 
-Plots.scatter(ci_filter[1:end-1]u"s", ci_filter[2:end]u"s")
+# finally a scatterplot of consecutive intervals, plot #8
+covmat = cov([ci_filter[1:end-1];; ci_filter[2:end]])
+Plots.scatter(ci_filter[1:end-1]u"s", ci_filter[2:end]u"s", aspect_ratio=:equal, xlim=(0, 0.14), ylim=(0, 0.14), title="consecutive intervals\nR2 score of $(covmat[2]^2 / covmat[1] / covmat[4])")
+using StatsPlots
+covellipse!([mean(ci_filter), mean(ci_filter)], covmat)
+
